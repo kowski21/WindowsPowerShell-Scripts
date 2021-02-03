9569954e4d7222666a9e8f4ad54e43ad180a758c
kowski21 <33750472+kowski21@users.noreply.github.com>
2021-02-03 08:09:27 -0500

    Delete Untitled2.ps1

--- Untitled2.ps1
+++
@@ -1,9 +0,0 @@
-﻿Invoke-Command -Credential $c -ComputerName $fqdn -ScriptBlock ${function:path-test}
-
-$c = Get-Credential
-$fqdn = "HQUWGEOCXMD0066"
-$IP = "10.28.12.137"
-
-
-#Kovitz's Machine
-HQUWGEOCXMD0066 
